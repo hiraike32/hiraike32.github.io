1da1d3fcd7a0d386f90252d11c3cc79987d5f7a7
PC-HIRAIKE\ETIC <sssb.th.63332@gmail.com>
2017-11-28 17:49:47 +0900

    サイトデザインを、「テキストエディタ風」に大きく変更。大まかな改修を行う。

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-$('#about, #works').css('visibility','hidden');
+$(function(){
 
-$(window).scroll(function(){
-	var windowHeight = $(window).height(),
-    	topWindow = $(window).scrollTop();
-	$('#about').each(function(){
-  		var targetPosition = $(this).offset().top;
-  		if(topWindow > targetPosition - windowHeight + 200){
-   			$(this).addClass("fadeInDown");
-   			$('body').animate({background:'#000'}, 'slow');
-  		}
-  	});
-  	$('#works').each(function(){
-  		var targetPosition = $(this).offset().top;
-  		if(topWindow > targetPosition - windowHeight + 200){
-   			$(this).addClass("fadeInDown");
- 		}
-	});
+  //スクロール時にコンテンツを出現させる
+  $('#about, #works').css('visibility','hidden');
+  $(window).scroll(function(){
+    var windowHeight = $(window).height(),
+        topWindow = $(window).scrollTop();
+    $('#about').each(function(){
+        var targetPosition = $(this).offset().top;
+        if(topWindow > targetPosition - windowHeight + 200){
+          $(this).addClass("fadeInDown");
+        }
+    });
+    $('#works').each(function(){
+        var targetPosition = $(this).offset().top;
+        if(topWindow > targetPosition - windowHeight + 200){
+          $(this).addClass("fadeInDown");
+        }
+    });
+  });
 
-	$('#works').each(function(){
-  		var targetPosition = $(this).offset().top;
-  		if(topWindow > targetPosition - windowHeight + 200){
-   			$(this).addClass("fadeInDown");
- 		}
-	});
+
 });
+
